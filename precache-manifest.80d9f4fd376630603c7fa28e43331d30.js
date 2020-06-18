495c63049993f5d9cffeb02ed23a41dee9bba901
nicholassiebenaller <nicholaspaulsiebenaller@gmail.com>
2020-06-18 13:49:01 -0400

    change homepage of app

---
+++ precache-manifest.80d9f4fd376630603c7fa28e43331d30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1b72fb4f37e7bd1429dfccc9696d02a",
+    "url": "/ui-components-demo/index.html"
+  },
+  {
+    "revision": "524ee5767b108c325290",
+    "url": "/ui-components-demo/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "a5492c49511b2c9c367d",
+    "url": "/ui-components-demo/static/css/main.b8d41071.chunk.css"
+  },
+  {
+    "revision": "524ee5767b108c325290",
+    "url": "/ui-components-demo/static/js/2.f97c2d06.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ui-components-demo/static/js/2.f97c2d06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5492c49511b2c9c367d",
+    "url": "/ui-components-demo/static/js/main.fcc5d13e.chunk.js"
+  },
+  {
+    "revision": "8cbf248fc21292666e84",
+    "url": "/ui-components-demo/static/js/runtime-main.e522851e.js"
+  }
+]);
