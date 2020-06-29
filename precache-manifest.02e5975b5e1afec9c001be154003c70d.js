95f4f8d3c3dacde10cb823b02ae28430cc185cfb
nicholassiebenaller <nicholaspaulsiebenaller@gmail.com>
2020-06-29 12:59:13 -0400

    build 1.0.5 - colors overhaul

---
+++ precache-manifest.02e5975b5e1afec9c001be154003c70d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a791bc0d59097bf6c4c1fe8a65a39100",
+    "url": "/ui-components-demo/index.html"
+  },
+  {
+    "revision": "2adb0955dc172284b15f",
+    "url": "/ui-components-demo/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "af5305cfda6c1d479319",
+    "url": "/ui-components-demo/static/css/main.c33d8f78.chunk.css"
+  },
+  {
+    "revision": "2adb0955dc172284b15f",
+    "url": "/ui-components-demo/static/js/2.fcf70002.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ui-components-demo/static/js/2.fcf70002.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af5305cfda6c1d479319",
+    "url": "/ui-components-demo/static/js/main.20352ca2.chunk.js"
+  },
+  {
+    "revision": "8cbf248fc21292666e84",
+    "url": "/ui-components-demo/static/js/runtime-main.e522851e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ui-components-demo/precache-manifest.2c022212f705066dfa8418a981c3d57d.js"
+  "/ui-components-demo/precache-manifest.02e5975b5e1afec9c001be154003c70d.js"
 );
 
 self.addEventListener('message', (event) => {
